6bae64466d5085e405540fa0b701e801d3f8dddf
Win <synwe@yahoo.com>
2019-08-09 11:52:44 +0000

    Linting

--- lib/src/geodesy.dart
+++ lib/src/geodesy.dart
@@ -1,15 +1,16 @@
 import 'dart:math' as math;
 import 'package:latlong/latlong.dart';
 
+/// The main geodesy class
 class Geodesy {
-  final num RADIUS = 6371e3; // meters
-  final num PI = math.pi;
+  final num _RADIUS = 6371e3; // meters
+  final num _PI = math.pi;
 
   /// calculate a destination point given the distance and bearing
   LatLng destinationPointByDistanceAndBearing(
       LatLng l, num distance, num bearing,
       [num radius]) {
-    radius = radius ?? RADIUS;
+    radius = radius ?? _RADIUS;
 
     num angularDistanceRadius = distance / radius;
     num bearingRadians = degToRadian(bearing as double);
@@ -30,7 +31,7 @@ class Geodesy {
     num y = sinBearingRadians * sinAngularDistanceRadius * cosLatRadians;
     num x = cosAngularDistanceRadius - sinLatRadians * sinLatRadians2;
     num lngRadians2 = lngRadians + math.atan2(y, x);
-    return new LatLng(radianToDeg(latRadians2 as double),
+    return LatLng(radianToDeg(latRadians2 as double),
         (radianToDeg(lngRadians2 as double) + 540) % 360 - 180);
   }
 
@@ -52,7 +53,7 @@ class Geodesy {
     num lngRadians =
         l1LngRadians + math.atan2(vectorY, math.cos(l1LatRadians) + vectorX);
 
-    return new LatLng(radianToDeg(latRadians as double),
+    return LatLng(radianToDeg(latRadians as double),
         (radianToDeg(lngRadians as double) + 540) % 360 - 180);
   }
 
@@ -86,9 +87,9 @@ class Geodesy {
 
     num finalBearingX = math.sin(l2LngRadians - l1LngRadians) > 0
         ? initBearingX
-        : 2 * PI - initBearingX;
+        : 2 * _PI - initBearingX;
     num finalBearingY = math.sin(l2LngRadians - l1LngRadians) > 0
-        ? 2 * PI - initBearingY
+        ? 2 * _PI - initBearingY
         : initBearingY;
 
     num angle1 = b1Radians - finalBearingX;
@@ -109,13 +110,13 @@ class Geodesy {
         math.cos(dst13) - math.sin(l1LatRadians) * math.sin(lat3));
     num l3LngRadians = l1LngRadians + lngRadiansDiff13;
 
-    return new LatLng(radianToDeg(lat3 as double),
+    return LatLng(radianToDeg(lat3 as double),
         (radianToDeg(l3LngRadians as double) + 540) % 360 - 180);
   }
 
   /// calculate the distance in meters between two geo points
   num distanceBetweenTwoGeoPoints(LatLng l1, LatLng l2, [num radius]) {
-    radius = radius ?? RADIUS;
+    radius = radius ?? _RADIUS;
     num R = radius;
     num l1LatRadians = degToRadian(l1.latitude);
     num l1LngRadians = degToRadian(l1.longitude);
@@ -155,9 +156,10 @@ class Geodesy {
     return (bearingBetweenTwoGeoPoints(l2, l1) + 180) % 360;
   }
 
-  /// calculate signed distance from a geo point to greate circle with start and end points
+  /// calculate signed distance from a geo point
+  /// to greate circle with start and end points
   num crossTrackDistanceTo(LatLng l1, LatLng start, LatLng end, [num radius]) {
-    num R = radius ?? RADIUS;
+    num R = radius ?? _RADIUS;
 
     num distStartL1 = distanceBetweenTwoGeoPoints(start, l1, R) / R;
     num radiansStartL1 =
@@ -181,7 +183,8 @@ class Geodesy {
         : false;
   }
 
-  /// check if a given geo point is in the a polygon using even-odd rule algorithm
+  /// check if a given geo point is in the a polygon
+  /// using even-odd rule algorithm
   bool isGeoPointInPolygon(LatLng l, List<LatLng> polygon) {
     var isInPolygon = false;
 
