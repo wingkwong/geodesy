3f9892f7d6cc6a022127b6eae8c083bdb3f014a9
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-04 12:23:57 +0800

    Revise test cases

--- test/geodesy_test.dart
+++ test/geodesy_test.dart
@@ -5,86 +5,88 @@ void main() {
   var geodesy = Geodesy();
 
   test('distanceBetweenTwoGeoPoints', () async {
-    final l1 = LatLng(50.06638889, 5.71472222);
-    final l2 = LatLng(58.64388889, 3.07000000);
+    final l1 = const LatLng(50.06638889, 5.71472222);
+    final l2 = const LatLng(58.64388889, 3.07000000);
     final distance = geodesy.distanceBetweenTwoGeoPoints(l1, l2);
-    expect(distance, 968853.5466871752);
+    final expectedDistance = 968853.5466871752;
+    expect(distance, expectedDistance);
   });
 
-  test('destinationPointByDistanceAndBearing', () async {
-    final l3 = LatLng(51.4778, -0.0015);
+  test('destinationPointByDistanceAndBearing', () {
+    final l3 = const LatLng(51.4778, -0.0015);
     final destinationPoint =
         geodesy.destinationPointByDistanceAndBearing(l3, 7794.0, 300.7);
-    expect(destinationPoint, LatLng(51.51354569174267, -0.0983453814483255));
+    final expectedDestinationPoint = const LatLng(51.513546, -0.098345);
+    expect(destinationPoint.toString(), expectedDestinationPoint.toString());
   });
 
   test('bearingBetweenTwoGeoPoints', () async {
-    final l4 = LatLng(52.205, 0.119);
-    final l5 = LatLng(48.857, 2.351);
+    final l4 = const LatLng(52.205, 0.119);
+    final l5 = const LatLng(48.857, 2.351);
     final bearing = geodesy.bearingBetweenTwoGeoPoints(l4, l5);
-    expect(bearing, 156.16658258153166);
+    final expectedBearing = 156.16658258153177;
+    expect(bearing, expectedBearing);
   });
 
-  test('bearingBetweenTwoGeoPoints', () async {
-    final l4 = LatLng(52.205, 0.119);
-    final l5 = LatLng(48.857, 2.351);
+  test('finalBearingBetweenTwoGeoPoints', () async {
+    final l4 = const LatLng(52.205, 0.119);
+    final l5 = const LatLng(48.857, 2.351);
     final finalBearing = geodesy.finalBearingBetweenTwoGeoPoints(l4, l5);
-    expect(finalBearing, 157.89044019049243);
+    final expectedFinalBearing = 157.89044019049243;
+    expect(finalBearing, expectedFinalBearing);
   });
 
   test('midPointBetweenTwoGeoPoints', () async {
-    final l4 = LatLng(52.205, 0.119);
-    final l5 = LatLng(48.857, 2.351);
+    final l4 = const LatLng(52.205, 0.119);
+    final l5 = const LatLng(48.857, 2.351);
     final midpoint = geodesy.midPointBetweenTwoGeoPoints(l4, l5);
-    expect(midpoint, LatLng(50.53632687827432, 1.2746141006782636));
-  });
-
-  test('midPointBetweenTwoGeoPoints', () async {
-    final l4 = LatLng(52.205, 0.119);
-    final l5 = LatLng(48.857, 2.351);
-    final midpoint = geodesy.midPointBetweenTwoGeoPoints(l4, l5);
-    expect(midpoint, LatLng(50.53632687827432, 1.2746141006782636));
+    final expectedMidpoint = const LatLng(50.536327, 1.274614);
+    expect(midpoint.toString(), expectedMidpoint.toString());
   });
 
   test('isGeoPointInBoudingBox', () async {
-    final l3 = LatLng(51.4778, -0.0015);
-    final l4 = LatLng(52.205, 0.119);
-    final l5 = LatLng(48.857, 2.351);
+    final l3 = const LatLng(51.4778, -0.0015);
+    final l4 = const LatLng(52.205, 0.119);
+    final l5 = const LatLng(48.857, 2.351);
     final inBoudingBox = geodesy.isGeoPointInBoudingBox(l3, l5, l4);
     expect(inBoudingBox, false);
   });
 
   test('intersectionByPaths', () async {
-    final l4 = LatLng(52.205, 0.119);
-    final l5 = LatLng(48.857, 2.351);
+    final l4 = const LatLng(52.205, 0.119);
+    final l5 = const LatLng(48.857, 2.351);
     final b1 = 108.547;
     final b2 = 32.435;
     final intersectionByPaths = geodesy.intersectionByPaths(l4, l5, b1, b2);
-    expect(intersectionByPaths, LatLng(51.15151144654275, 4.698604211862175));
+    final expectedIntersectionByPaths =
+        const LatLng(51.15151144654275, 4.698604211862175);
+    expect(
+        intersectionByPaths.toString(), expectedIntersectionByPaths.toString());
   });
 
   test('crossTrackDistanceTo', () async {
-    final l4 = LatLng(52.205, 0.119);
-    final l5 = LatLng(48.857, 2.351);
-    final l6 = LatLng(50.587, 1.231);
+    final l4 = const LatLng(52.205, 0.119);
+    final l5 = const LatLng(48.857, 2.351);
+    final l6 = const LatLng(50.587, 1.231);
     final distanceToGreatCircle = geodesy.crossTrackDistanceTo(l4, l5, l6);
-    expect(distanceToGreatCircle, 1240.3379338876143);
+    final expectedDistanceToGreatCircle = 1240.3379338876143;
+    expect(distanceToGreatCircle, expectedDistanceToGreatCircle);
   });
 
   test('crossTrackDistanceTo', () async {
     var poly = <LatLng>[
-      LatLng(1.0, 1.0),
-      LatLng(1.0, 2.0),
-      LatLng(2.0, 2.0),
-      LatLng(2.0, 1.0)
+      const LatLng(1.0, 1.0),
+      const LatLng(1.0, 2.0),
+      const LatLng(2.0, 2.0),
+      const LatLng(2.0, 1.0)
     ];
-    var l7 = LatLng(1.5, 1.5);
+    var l7 = const LatLng(1.5, 1.5);
     var isGeoPointInPolygon = geodesy.isGeoPointInPolygon(l7, poly);
     expect(isGeoPointInPolygon, true);
   });
 
   test('pointsInRange', () async {
-    final point = LatLng(51.0, 0);
+    final point = const LatLng(51.0, 0);
     final distance = 10000;
     final pointNotInRange = geodesy.destinationPointByDistanceAndBearing(
         point, distance + 10, 420.0);
