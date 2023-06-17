3806a2f7d2961e53f3f3f16302da1a39ab7acb36
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-18 01:02:42 +0800

    cleanup calculateBoundingBox

--- lib/src/geodesy.dart
+++ lib/src/geodesy.dart
@@ -285,23 +285,19 @@ class Geodesy {
   /// Bounding Box per distance in Kilometers
   List<LatLng> calculateBoundingBox(LatLng centerPoint, num distanceInKm) {
     // Earth's radius in kilometers
-    final num radiusOfEarth = 6371.0;
+    final num radiusOfEarth = _RADIUS / 1000;
     // Convert latitude to radians
-    final num latInRadians = centerPoint.latitude * (math.pi / 180.0);
-
+    final num latInRadians = centerPoint.latitude * (_PI / 180.0);
     final num degreeLatDistance =
-        (distanceInKm / radiusOfEarth) * (180.0 / math.pi);
+        (distanceInKm / radiusOfEarth) * (180.0 / _PI);
     final num degreeLngDistance = degreeLatDistance / math.cos(latInRadians);
-
     final num topLat = centerPoint.latitude + degreeLatDistance;
     final num leftLng = centerPoint.longitude - degreeLngDistance;
     final num bottomLat = centerPoint.latitude - degreeLatDistance;
     final num rightLng = centerPoint.longitude + degreeLngDistance;
-
     final LatLng topLeft = LatLng(topLat.toDouble(), leftLng.toDouble());
     final LatLng bottomRight =
         LatLng(bottomLat.toDouble(), rightLng.toDouble());
-
     return [topLeft, bottomRight];
   }
 }
